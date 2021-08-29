b18a58832d3e727f334f9e932e531326f5fcb59f
datree-ci <rnd@datree.io>
2021-08-29 17:39:12 +0300

    Brew formula update for datree-cli version 0.8.2-DAT-3412-fix-brew-version

--- datree.rb
+++ datree.rb
@@ -10,15 +10,15 @@ class Datree < Formula
 
   if OS.mac?
     url "https://github.com/datreeio/datree/releases/download/0.8.2-DAT-3412-fix-brew-version/datree-cli_0.8.2-DAT-3412-fix-brew-version_Darwin_x86_64.zip"
-    sha256 "04ee6445df78d0d6c79765381680bcfae4b593e2b0ac0b5a4fe1564c15b79a7b"
+    sha256 "bdba86d76d7b64650e9df840bfaa3cdc0d2692a0fd9be66fdc73ab62384fd1b2"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/datreeio/datree/releases/download/0.8.2-DAT-3412-fix-brew-version/datree-cli_0.8.2-DAT-3412-fix-brew-version_Linux_x86_64.zip"
-    sha256 "95822832eae1416442f67e0374d1447addcc9896441393cb47ebc9fac3d44be3"
+    sha256 "48b6f1971d2f736cb5f8abc2cfe3c428257f71140a1611c28aff9898f50741f5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/datreeio/datree/releases/download/0.8.2-DAT-3412-fix-brew-version/datree-cli_0.8.2-DAT-3412-fix-brew-version_Linux_arm64.zip"
-    sha256 "0ed1ea833563931eb594fb5e44a54ff61f755422c7556f8c5054008d2fd392f5"
+    sha256 "67768ad3a77efe1b1e0006f7add8fd722e7dfe77990c0a719be5345ac653e983"
   end
 
   def install
