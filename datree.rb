37a2ad89edaa98c6e3bb578c732483cdbbb81ffb
Travis CI <builds@travis-ci.com>
2021-09-01 17:40:38 +0000

    Brew formula update for datree-cli version 0.9.4-DAT-3420-remove-find-replace

--- datree.rb
+++ datree.rb
@@ -4,20 +4,20 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "0.9.3-DAT-3420-remove-find-replace"
+  version "0.9.4-DAT-3420-remove-find-replace"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/datreeio/datree/releases/download/0.9.3-DAT-3420-remove-find-replace/datree-cli_0.9.3-DAT-3420-remove-find-replace_Darwin_x86_64.zip"
-    sha256 "79f1b4e98c2f9341496cbbf5557d8774dbbc8e9e96a38a6f32da039050d3679d"
+    url "https://github.com/datreeio/datree/releases/download/0.9.4-DAT-3420-remove-find-replace/datree-cli_0.9.4-DAT-3420-remove-find-replace_Darwin_x86_64.zip"
+    sha256 "fa49097e50afd57d88ce867c6a0a0a54f93e7a08f41b7415b2d1018a36483b57"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.9.3-DAT-3420-remove-find-replace/datree-cli_0.9.3-DAT-3420-remove-find-replace_Linux_x86_64.zip"
-    sha256 "73aae467ef3b090758246881938b83bd9332276d58f1c6079838504825202710"
+    url "https://github.com/datreeio/datree/releases/download/0.9.4-DAT-3420-remove-find-replace/datree-cli_0.9.4-DAT-3420-remove-find-replace_Linux_x86_64.zip"
+    sha256 "d87322bd69a53b2ee8be2dcf66461e8e04ecc511f68dc8eb165d54a5cc797995"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/0.9.3-DAT-3420-remove-find-replace/datree-cli_0.9.3-DAT-3420-remove-find-replace_Linux_arm64.zip"
-    sha256 "464567d0a500d1ad63662b5a1d14636aa408fb4d0ac3ad612e3110f24e5b2495"
+    url "https://github.com/datreeio/datree/releases/download/0.9.4-DAT-3420-remove-find-replace/datree-cli_0.9.4-DAT-3420-remove-find-replace_Linux_arm64.zip"
+    sha256 "099373a0d82638e7b6ea36fb9ddda2eb2576bb35801384811857e102501fc2ad"
   end
 
   def install
