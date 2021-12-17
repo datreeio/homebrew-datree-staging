4fda25e88ca450702e4de1d07a799e460d9bb21d
Travis CI <builds@travis-ci.com>
2021-12-17 15:45:43 +0000

    Brew formula update for datree-cli version 0.14.83-rc

--- datree.rb
+++ datree.rb
@@ -4,19 +4,19 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "0.14.82-rc"
+  version "0.14.83-rc"
 
   if OS.mac?
-    url "https://github.com/datreeio/datree/releases/download/0.14.82-rc/datree-cli_0.14.82-rc_Darwin_x86_64.zip"
-    sha256 "9b2a330c69527f4d5134737ae674df98caa3d45582385a32163ca5055bbf1fd0"
+    url "https://github.com/datreeio/datree/releases/download/0.14.83-rc/datree-cli_0.14.83-rc_Darwin_x86_64.zip"
+    sha256 "c4dc4dcf0a0fb74bedba00a32b663ffd74187e821c1c19a1797912d568669886"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.14.82-rc/datree-cli_0.14.82-rc_Linux_x86_64.zip"
-    sha256 "40d09e5bc9540d79bfe95f94499ee298d5ea614ec2dbc10f15fd63115c3ed9fd"
+    url "https://github.com/datreeio/datree/releases/download/0.14.83-rc/datree-cli_0.14.83-rc_Linux_x86_64.zip"
+    sha256 "8b6f209ae95b6c0f3fabcaa998477d9e8f60711daab126d61ce1de3ea238afe5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/0.14.82-rc/datree-cli_0.14.82-rc_Linux_arm64.zip"
-    sha256 "ea159c4d20acf0e2e4009d7b01668e8b72c514ef0bca99825c0eb00f69600dbf"
+    url "https://github.com/datreeio/datree/releases/download/0.14.83-rc/datree-cli_0.14.83-rc_Linux_arm64.zip"
+    sha256 "50ae676ce3ab7566333a9f68a93efe069e4404304bb88973cefa359f8ab57357"
   end
 
   def install
