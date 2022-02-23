9975efbec1ed4c935a202ef7316d2c1fb2c0e901
Travis CI <builds@travis-ci.com>
2022-02-23 15:11:23 +0000

    Brew formula update for datree-cli version 0.15.26-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "0.15.25-rc"
+  version "0.15.26-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.15.25-rc/datree-cli_0.15.25-rc_Darwin_x86_64.zip"
-    sha256 "fc029c0eedb2b2c65a7deffb41458528037d0e9b076caa4e5e6e56edad92d3e7"
+    url "https://github.com/datreeio/datree/releases/download/0.15.26-rc/datree-cli_0.15.26-rc_Darwin_x86_64.zip"
+    sha256 "750ce6574e8303c876f0e567fa06606a59d4b4ffb5a76647dc4e6afa76d8d08a"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/0.15.25-rc/datree-cli_0.15.25-rc_Darwin_arm64.zip"
-    sha256 "483c0e7998a684dfbf0a963c27b72f690486bc56dce6ff407fdba47572b1e15f"
+    url "https://github.com/datreeio/datree/releases/download/0.15.26-rc/datree-cli_0.15.26-rc_Darwin_arm64.zip"
+    sha256 "f8df1ddb8b75a92831852faac8971269db93b5f7dfdd8b75716a151dc9c8e6f1"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.15.25-rc/datree-cli_0.15.25-rc_Linux_x86_64.zip"
-    sha256 "677a169ac1b0a4a28e67eb19dd6bf564dd0329ac3553d9ee96c178be370e1ce0"
+    url "https://github.com/datreeio/datree/releases/download/0.15.26-rc/datree-cli_0.15.26-rc_Linux_x86_64.zip"
+    sha256 "b507e4c5fba72a5e05a927389aa078ff4ef6e046998a2668fdac5ae705bdafa2"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/0.15.25-rc/datree-cli_0.15.25-rc_Linux_arm64.zip"
-    sha256 "1887343a09cf97d8ac9e832e39261f3e677959500bcab037e4b73a716ced4eee"
+    url "https://github.com/datreeio/datree/releases/download/0.15.26-rc/datree-cli_0.15.26-rc_Linux_arm64.zip"
+    sha256 "f7c24972ada1b6f3fe00cedf314d0310c202bdb3e1866d75fb56c4e83b473b89"
   end
 
   def install
