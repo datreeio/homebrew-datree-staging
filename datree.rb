4ffc76e620b7fd489efdfa1bbdc31fd0b3d3e055
Travis CI <builds@travis-ci.com>
2022-03-01 14:25:40 +0000

    Brew formula update for datree-cli version 0.15.32-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "0.15.31-rc"
+  version "0.15.32-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.15.31-rc/datree-cli_0.15.31-rc_Darwin_x86_64.zip"
-    sha256 "b80baf98eb7c4d7f894d6c81e6a04bb0428c514e1b3f0f16dde090c64b283c19"
+    url "https://github.com/datreeio/datree/releases/download/0.15.32-rc/datree-cli_0.15.32-rc_Darwin_x86_64.zip"
+    sha256 "7317e2eb32ace09865b68541b2190b3b74373645b31a941a1e664bfae9657180"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/0.15.31-rc/datree-cli_0.15.31-rc_Darwin_arm64.zip"
-    sha256 "6bfd3fe867e6737c97ab80a6e5d592085f87575087ab8c9d91fa58dbad7d085a"
+    url "https://github.com/datreeio/datree/releases/download/0.15.32-rc/datree-cli_0.15.32-rc_Darwin_arm64.zip"
+    sha256 "baf03b588d367069a7ba354b5b32aa9f41fbcb831cb1691cce8ae059d6a6a8ce"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.15.31-rc/datree-cli_0.15.31-rc_Linux_x86_64.zip"
-    sha256 "af4a4858aa5b3a2fa9111a32b7ecfce894533c629f78034d68867461df40dadf"
+    url "https://github.com/datreeio/datree/releases/download/0.15.32-rc/datree-cli_0.15.32-rc_Linux_x86_64.zip"
+    sha256 "8e0c88bcbbcc42cc1de3c7d7b97a3aee4847032c36d2839ce3bb2682cbc811d4"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/0.15.31-rc/datree-cli_0.15.31-rc_Linux_arm64.zip"
-    sha256 "083ade887cbfe71f283391c16dabeb2f377ffbbc93bda7aa5877eec4a17f1ac8"
+    url "https://github.com/datreeio/datree/releases/download/0.15.32-rc/datree-cli_0.15.32-rc_Linux_arm64.zip"
+    sha256 "19a953bcfa8b765fe54b777f309eda83c3e4c6c3bd28bb2abc3a2a32d7bd2418"
   end
 
   def install
