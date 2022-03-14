146bd7a45a388c6d7b967a98a25991793bcc51a8
Travis CI <builds@travis-ci.com>
2022-03-14 19:07:42 +0000

    Brew formula update for datree-cli version 0.16.12-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "0.16.11-rc"
+  version "0.16.12-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.16.11-rc/datree-cli_0.16.11-rc_Darwin_x86_64.zip"
-    sha256 "1ee9f36c4657e01398e25998b3e288089f43a377565bf500d2ae406812f49f0d"
+    url "https://github.com/datreeio/datree/releases/download/0.16.12-rc/datree-cli_0.16.12-rc_Darwin_x86_64.zip"
+    sha256 "4250b3f4edc773bd2865e421aa4a6168050f485272039c39984180c699bb937d"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/0.16.11-rc/datree-cli_0.16.11-rc_Darwin_arm64.zip"
-    sha256 "aad58588939eccfc4ca1bdee02459368e7f03ac9c39a9210154347932543ceca"
+    url "https://github.com/datreeio/datree/releases/download/0.16.12-rc/datree-cli_0.16.12-rc_Darwin_arm64.zip"
+    sha256 "7b62ee06ca270780eca2bad06e1c5f820b2cf4051d9f0a3e571773d16bfa6379"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/0.16.11-rc/datree-cli_0.16.11-rc_Linux_x86_64.zip"
-    sha256 "60bf3b7f7c93a8f32336a8842c52d48456250fe3e6e8e6bb5204de6581687292"
+    url "https://github.com/datreeio/datree/releases/download/0.16.12-rc/datree-cli_0.16.12-rc_Linux_x86_64.zip"
+    sha256 "1ddb8773e6a5ad59144d22f5f8b8c889880065fc32124ec823850950bc412038"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/0.16.11-rc/datree-cli_0.16.11-rc_Linux_arm64.zip"
-    sha256 "9427995035cf57b0345889fd3f72142726882e98159375609dcb3899055c8087"
+    url "https://github.com/datreeio/datree/releases/download/0.16.12-rc/datree-cli_0.16.12-rc_Linux_arm64.zip"
+    sha256 "53bb21045aba915f7809dbe0b5ed8a75f4b4f79044c38f318deccf54eb11670d"
   end
 
   def install
