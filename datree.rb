eaf088cc72e2a441069264183d233374bbb495f9
Travis CI <builds@travis-ci.com>
2022-03-21 11:09:04 +0000

    Brew formula update for datree-cli version 1.0.10-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.0.10-yishay-rc"
+  version "1.0.10-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.0.10-yishay-rc/datree-cli_1.0.10-yishay-rc_Darwin_x86_64.zip"
-    sha256 "c0ac3ee1b6d17134e8cb45dcec1f8f43ba95ef8b02e47def7b77b4323282ac17"
+    url "https://github.com/datreeio/datree/releases/download/1.0.10-rc/datree-cli_1.0.10-rc_Darwin_x86_64.zip"
+    sha256 "37d1deca7e0b904ea8566693ca9cb7ed426b562635847eaff1bba82c694be0a0"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.0.10-yishay-rc/datree-cli_1.0.10-yishay-rc_Darwin_arm64.zip"
-    sha256 "405ec7477d98be32d117b37e1e39797108a238a756f9cd364a9383c5ee41d5da"
+    url "https://github.com/datreeio/datree/releases/download/1.0.10-rc/datree-cli_1.0.10-rc_Darwin_arm64.zip"
+    sha256 "740546744914f95c8a3d9d4929778d147e1d94f9da26a7792f3bea11ba2bf09a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.0.10-yishay-rc/datree-cli_1.0.10-yishay-rc_Linux_x86_64.zip"
-    sha256 "1e9bcbbcc2ff2db202ba7b0d2f916cb154fd03c8da63c23bb4c2cb9f232d403c"
+    url "https://github.com/datreeio/datree/releases/download/1.0.10-rc/datree-cli_1.0.10-rc_Linux_x86_64.zip"
+    sha256 "9c35f7c642846b956507b895726ddb03d76842f1e9a80c22689c37f5ce555125"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.0.10-yishay-rc/datree-cli_1.0.10-yishay-rc_Linux_arm64.zip"
-    sha256 "b963f29e84279bc435f395fe5b96211bfe6022c2fcc9d4162318ef9e74236ed4"
+    url "https://github.com/datreeio/datree/releases/download/1.0.10-rc/datree-cli_1.0.10-rc_Linux_arm64.zip"
+    sha256 "ddd86b6a0b2bac875b801f8ad1c78c582d746a640ff30bd0e9805e23c18531ed"
   end
 
   def install
