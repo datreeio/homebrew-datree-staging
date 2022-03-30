ac0f8bd0ef78ffde096afa5cd9b604a7194100a9
Travis CI <builds@travis-ci.com>
2022-03-30 12:49:47 +0000

    Brew formula update for datree-cli version 1.1.8-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.1.7-rc"
+  version "1.1.8-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.1.7-rc/datree-cli_1.1.7-rc_Darwin_x86_64.zip"
-    sha256 "0dc5f0f9559c5c2bcd46bc06733cc954d269a61e4d6265ffbb81cbd456c2fc28"
+    url "https://github.com/datreeio/datree/releases/download/1.1.8-rc/datree-cli_1.1.8-rc_Darwin_x86_64.zip"
+    sha256 "8f2fdfbf338104bb50036c8e65722692bbe2b352c3c2745e518d15da988bae05"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.1.7-rc/datree-cli_1.1.7-rc_Darwin_arm64.zip"
-    sha256 "3d0afea164c6bcc93603dd5b35cc4c2a54eebfb1b9263da0a3c28a4edef1d421"
+    url "https://github.com/datreeio/datree/releases/download/1.1.8-rc/datree-cli_1.1.8-rc_Darwin_arm64.zip"
+    sha256 "dcf05a47a164c360b719115b0cab47276c328276a4ef4f3cdcf4030e5570cc17"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.1.7-rc/datree-cli_1.1.7-rc_Linux_x86_64.zip"
-    sha256 "c94ef5255e1262bf2e480c143fb3eeee656faba1a275c3954219f697a223500a"
+    url "https://github.com/datreeio/datree/releases/download/1.1.8-rc/datree-cli_1.1.8-rc_Linux_x86_64.zip"
+    sha256 "f16d6f575686afc80839762084cfaf61d03737056d9e042686a2e21749292a13"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.1.7-rc/datree-cli_1.1.7-rc_Linux_arm64.zip"
-    sha256 "e8b27038647b41b88a54221f9476e461ad9edf130aae65869352aac449cf8ad4"
+    url "https://github.com/datreeio/datree/releases/download/1.1.8-rc/datree-cli_1.1.8-rc_Linux_arm64.zip"
+    sha256 "03f8e809ed3a544970d54b7dff5e1ed0812f6d68165160239bedbea59dff95a5"
   end
 
   def install
@@ -35,7 +35,6 @@ class Datree < Formula
       \033[35m Usage: $ datree test [k8s-file.yaml]
        Using Helm? => https://github.com/datreeio/helm-datree
        Using Kustomize? => https://hub.datree.io/kustomize-support
-      tput init
     EOS
   end
 end
