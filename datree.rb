483d13aa45515459a345046e6cc7305dd99501dc
Travis CI <builds@travis-ci.com>
2022-04-03 12:46:37 +0000

    Brew formula update for datree-cli version 1.1.16-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.1.15-rc"
+  version "1.1.16-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.1.15-rc/datree-cli_1.1.15-rc_Darwin_x86_64.zip"
-    sha256 "efa18b032755ab45d7a56c1d13df435145c619a7a823fa5e1521d8d1de4bb3f2"
+    url "https://github.com/datreeio/datree/releases/download/1.1.16-rc/datree-cli_1.1.16-rc_Darwin_x86_64.zip"
+    sha256 "d9e8ef2337bff0ad470cb179dcb9cd92e36007a2fa18e4f9aa3c5285aa0ba4fa"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.1.15-rc/datree-cli_1.1.15-rc_Darwin_arm64.zip"
-    sha256 "941766549d98ad15fdf1ba6b24eca6ce23dc30b8151088390dfb3480563fd10b"
+    url "https://github.com/datreeio/datree/releases/download/1.1.16-rc/datree-cli_1.1.16-rc_Darwin_arm64.zip"
+    sha256 "3f636f4db4a8b89a01aa2c948b8091b1ae545f348260d937c00dd0d87033d47f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.1.15-rc/datree-cli_1.1.15-rc_Linux_x86_64.zip"
-    sha256 "5a0ce54c8aaa4171c6c992fb5e02a6b10bda98da7e3590b0e4473451d5cff0f4"
+    url "https://github.com/datreeio/datree/releases/download/1.1.16-rc/datree-cli_1.1.16-rc_Linux_x86_64.zip"
+    sha256 "07b10b02387f51d54ecebaca2c933a17478a3f37d65373b60f0e73cd3ea8e1f2"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.1.15-rc/datree-cli_1.1.15-rc_Linux_arm64.zip"
-    sha256 "9e680c2a00579830d23e78ae30f558fbb35b5a47f20a8604e3c08910522099a2"
+    url "https://github.com/datreeio/datree/releases/download/1.1.16-rc/datree-cli_1.1.16-rc_Linux_arm64.zip"
+    sha256 "3a53e0bdd3197ec1e6836a875f18f0791a40f3be9423adf6bb62193b5f25629f"
   end
 
   def install
