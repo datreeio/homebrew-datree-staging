575ba223b297a266cbffb922748a6b1aabf9169e
Travis CI <builds@travis-ci.com>
2022-06-06 08:38:22 +0000

    Brew formula update for datree-cli version 1.4.40-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.4.40-rc-dima-test"
+  version "1.4.40-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc-dima-test/datree-cli_1.4.40-rc-dima-test_Darwin_x86_64.zip"
-    sha256 "87792ba3bf1db0cc2616f0c408ae05b9d65dfcd1f85914774987b9700690390c"
+    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc/datree-cli_1.4.40-rc_Darwin_x86_64.zip"
+    sha256 "15c63b5c816155a043a99f0a69887029e18e0e8c0eaf6fbc355530b398ac3010"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc-dima-test/datree-cli_1.4.40-rc-dima-test_Darwin_arm64.zip"
-    sha256 "a97484387c5d8f232b7821e674745700ec88378b21321acbe6a04c493ea26dc8"
+    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc/datree-cli_1.4.40-rc_Darwin_arm64.zip"
+    sha256 "e605ff00673bb0ec765a953678b4b39aa66a9f5ccc0fd13bb0e534d72fcf5ede"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc-dima-test/datree-cli_1.4.40-rc-dima-test_Linux_x86_64.zip"
-    sha256 "7248383ebf0c7479645439870e417a5d27837a475d337308f4a07cd46640d833"
+    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc/datree-cli_1.4.40-rc_Linux_x86_64.zip"
+    sha256 "1d6840f544bbc30f36202f123dbc1b5baa250808f62b96538ec8ab6098a74509"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc-dima-test/datree-cli_1.4.40-rc-dima-test_Linux_arm64.zip"
-    sha256 "c91571d9677e77c5ef5556c9039d3dd4ae3b1fbb02f4d7bd20f72b31d7285bb3"
+    url "https://github.com/datreeio/datree/releases/download/1.4.40-rc/datree-cli_1.4.40-rc_Linux_arm64.zip"
+    sha256 "2e446a963153cc1d940e1c2cb12f3132f76ce0a3dc329b4e95b00c6ad18b2747"
   end
 
   def install
