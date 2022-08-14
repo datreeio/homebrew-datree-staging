d756730078b475f47a12d80ee1221fb758c88c60
Travis CI <builds@travis-ci.com>
2022-08-14 09:26:47 +0000

    Brew formula update for datree-cli version 1.6.9-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.6.8-rc"
+  version "1.6.9-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.6.8-rc/datree-cli_1.6.8-rc_Darwin_x86_64.zip"
-    sha256 "b81ad091eec401f00b651790f27a85cd21a30f504b5367a3fc23c8ac3d6b5765"
+    url "https://github.com/datreeio/datree/releases/download/1.6.9-rc/datree-cli_1.6.9-rc_Darwin_x86_64.zip"
+    sha256 "c1764b0348b364ba7471cd8efe0d06b464f71ba11fe1ca5dca835e25224feec1"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.6.8-rc/datree-cli_1.6.8-rc_Darwin_arm64.zip"
-    sha256 "59ce55cd87c99732f6bce101c0607a0b69cc63da4395e9381670e9a49eb07e22"
+    url "https://github.com/datreeio/datree/releases/download/1.6.9-rc/datree-cli_1.6.9-rc_Darwin_arm64.zip"
+    sha256 "c49b5663c340c6cb6643a1f892a70bb761ebfd12e8888ae216ddeb3efa0cac15"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.6.8-rc/datree-cli_1.6.8-rc_Linux_x86_64.zip"
-    sha256 "d93675522ceb34eb44f85286d379bc919da0a42c72fdec9393e7a424609e7b22"
+    url "https://github.com/datreeio/datree/releases/download/1.6.9-rc/datree-cli_1.6.9-rc_Linux_x86_64.zip"
+    sha256 "eb9125d7b649b887137b18f687d1e8ff6c04dcfeb99539632ce7a7b99b64a93b"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.6.8-rc/datree-cli_1.6.8-rc_Linux_arm64.zip"
-    sha256 "a21627847c63e0c53106cc392400efb7e5eb55979493e6b9e1e4c11f0b34b254"
+    url "https://github.com/datreeio/datree/releases/download/1.6.9-rc/datree-cli_1.6.9-rc_Linux_arm64.zip"
+    sha256 "e22d4fdac2e1914699a58c6cbeff97134491e5a22d98b7bfad7d864ad52004d6"
   end
 
   def install
