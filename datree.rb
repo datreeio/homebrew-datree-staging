11a708eee0ab133f103be3bd849667baf1dc01d2
datree-ci <ci@datree.io>
2023-05-08 14:42:29 +0000

    Brew formula update for datree-cli version 1.8.70-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.8.69-rc"
+  version "1.8.70-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.8.69-rc/datree-cli_1.8.69-rc_Darwin_x86_64.zip"
-    sha256 "fa3e30e571a5e87832fe7814c54f8798b8e5d67568a9b189fc7962b511e0aa76"
+    url "https://github.com/datreeio/datree/releases/download/1.8.70-rc/datree-cli_1.8.70-rc_Darwin_x86_64.zip"
+    sha256 "5e3068d65aa0171cace0a1c1bc325192aab3ac86d821a40606a6d120c8e5be7a"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.8.69-rc/datree-cli_1.8.69-rc_Darwin_arm64.zip"
-    sha256 "6c96eb0169111ec65226373faa8202971655ef4b4b6a92b676e3ecb771cd03e3"
+    url "https://github.com/datreeio/datree/releases/download/1.8.70-rc/datree-cli_1.8.70-rc_Darwin_arm64.zip"
+    sha256 "c59f719d43314fdb661531de8b4dfc05c7ad080d6eb6781ed4e0d6c9596c4181"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.8.69-rc/datree-cli_1.8.69-rc_Linux_x86_64.zip"
-    sha256 "86196e5ed1efa3cbc4128b83f252c868650ee9d4d264f4de387271a7ea633eb5"
+    url "https://github.com/datreeio/datree/releases/download/1.8.70-rc/datree-cli_1.8.70-rc_Linux_x86_64.zip"
+    sha256 "afb4052cd8b00d95aad0983e96e154907fc7ceadf664c7797c6b677eae5b84a9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.8.69-rc/datree-cli_1.8.69-rc_Linux_arm64.zip"
-    sha256 "6327ce4c26beb4af08d4f32b352b427ad266581b1186621be351e5559e41de5e"
+    url "https://github.com/datreeio/datree/releases/download/1.8.70-rc/datree-cli_1.8.70-rc_Linux_arm64.zip"
+    sha256 "03985fab785cde57f820dfc6576659e9fedf6b0c659581423a2e3269bbcde806"
   end
 
   def install
