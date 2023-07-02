ba759f07c06a5181166723e3f2f2e2dc213cff85
datree-ci <ci@datree.io>
2023-07-02 14:15:13 +0000

    Brew formula update for datree-cli version 1.9.10-rc

--- datree.rb
+++ datree.rb
@@ -4,23 +4,23 @@
 class Datree < Formula
   desc ""
   homepage "https://datree.io/"
-  version "1.9.9-rc"
+  version "1.9.10-rc"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.9.9-rc/datree-cli_1.9.9-rc_Darwin_x86_64.zip"
-    sha256 "7204b4da2fc8dcbf604a5385514293da142911e2a8f192cf1abb53b75862a2e3"
+    url "https://github.com/datreeio/datree/releases/download/1.9.10-rc/datree-cli_1.9.10-rc_Darwin_x86_64.zip"
+    sha256 "0d518dab35bb45d4de50ba0f9968789daaff8584367218601b02f652d6a42228"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/datreeio/datree/releases/download/1.9.9-rc/datree-cli_1.9.9-rc_Darwin_arm64.zip"
-    sha256 "30ed88b7dfa1ea3e4dd5efed62a1fef7d6ad090f3490d7b7395103af345e5773"
+    url "https://github.com/datreeio/datree/releases/download/1.9.10-rc/datree-cli_1.9.10-rc_Darwin_arm64.zip"
+    sha256 "c42496b809faecc7a0a097faeb91ca34123f1f1d195902c2ed09e2636fa25e5a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/datreeio/datree/releases/download/1.9.9-rc/datree-cli_1.9.9-rc_Linux_x86_64.zip"
-    sha256 "bd5686cb84b72aef25776cd429a065e5b40bede3ce74a2ffce7e836e0ac2cd21"
+    url "https://github.com/datreeio/datree/releases/download/1.9.10-rc/datree-cli_1.9.10-rc_Linux_x86_64.zip"
+    sha256 "53c0a9b58b8b9d6ee063e2933e74bb11c4f5950de23bb1d10f18030a1d138717"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/datreeio/datree/releases/download/1.9.9-rc/datree-cli_1.9.9-rc_Linux_arm64.zip"
-    sha256 "af9857d436b50b2ad8559d2dae90e7036524d1d0ca86041316da224bddd40729"
+    url "https://github.com/datreeio/datree/releases/download/1.9.10-rc/datree-cli_1.9.10-rc_Linux_arm64.zip"
+    sha256 "b5ccf5f9fbda147c9afce84506733a292092234e9351e40e291c1c272205e7c0"
   end
 
   def install
